a5e4aba9e204923a741a095f9819a02bebd4f345
Kuswaa <kuswatulkaaba@gmail.com>
2025-04-25 10:19:34 +0500

    Firebase configured in project

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { SidenavComponent } from '../sidenav/sidenav.component';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SidenavComponent, HomeComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
